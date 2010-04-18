635c575e795f7cee93c9f13bb0b8e14281204b9e
snipercrazy37 <snipercrazy37@934d30a3-e6d5-50ff-10ec-50884c1415c3>
2010-04-18 15:29:29 +0000

    Ajout de quelques commentaires sur princ.cpp

--- trunk/OpenGL/scene/brouillon/TestVertex.cpp
+++ trunk/OpenGL/scene/brouillon/TestVertex.cpp
@@ -1,12 +1,16 @@
 #include "vertex.h"
-//#include "vertex.cpp"
+//#include "vertex.cpp" // pour que marc compile :P 
 
+// Fonction qui teste de quel côté du segment se situe un point
+// Si la fonction retourne 0 alors le point est sur le segment
 float droiteGauche(float x, float y, float x0,float y0, float x1, float y1){
 	return (y -y0)*(x1 - x0) - (x -x0)*(y1 - y0);
 }
 
-// retourne vrai si il est dedans ou sur la ligne
-// on suppose que l'on travaille avec des carres (le fond de la bounding box)
+// Retourne vrai si le point testé est dans le polygone (ici on a simplifié on travaille avec des carrés donc un polygone à 4 sommets)
+// L'algorithme va regarder où se situe le point par rapport aux 4 cotés du polygone si il est toujours du même côté alors le point est dans le polygone
+// Sinon le point est dehors.
+// On suppose que l'on travaille avec des carrés (le fond de la bounding box)
 bool vertexInsidePolyg(Vertex p1, Vertex p2, Vertex p3, Vertex p4, Vertex p){	// on teste si "p" est dans le carre p1p2p3p4
 
 	// PREMIER TEST
--- trunk/OpenGL/scene/princ.cpp
+++ trunk/OpenGL/scene/princ.cpp
@@ -22,15 +22,21 @@ int xCam=5;
 int yCam=5;
 
 
-struct vertex {
-    int num;
-    vector<float> coord;
-};
 
-struct forme {
-    vector< vertex > vertices;
-    vector< vector<int> > faces;
-};
+/*struct vertex {
+  int num;
+  vector<float> coord;
+  };
+
+  struct forme {
+  vector< vertex > vertices;
+  vector< vector<int> > faces;
+  };
+ */
+
+
+
+
 
 
 /**
@@ -38,115 +44,115 @@ struct forme {
  *	
  *	@param vFormes : vector<forme> * vFormes : permet de stocker les formes détectées 
  * */
- 
+
 int parser( string chemin, vector<Forme> * vFormes ) {
 
-    int pos;
-    string l;
+    int pos; // sert à rien ?
+    string l; // idem on peut s'en passer
     vector<int> vFaces;
-    int nv_forme=1;
+    int nv_forme=1; // nouvelle forme mais il faudrait utiliser un booléen ici puisque nv_forme = 1 ou 0
     Forme forme( 1 );
-    int cpt_vertex=1;
+    int cpt_vertex=1; // sert à compter le nombre de vertex trouvé
 
     //on ouvre le fichier
     ifstream fichier( chemin.c_str(), ios::in );
 
     //si l'ouverture du fichier s'est bien passée
     if ( fichier ) {
-        string ligne;
-        //tant qu'on est pas arrivé à la fin du fichier
-        while ( !fichier.eof() ) {
-            //on récupère toute la ligne
-            getline( fichier, ligne );
-            //on teste la première lettre de la ligne
-            switch ( ligne[0] ) {
-                //s'il s'agit d'un vertex
-                case 'v':
-                    {
-                        //s'il s'agit d'une nouvelle forme
-                        if ( nv_forme == 0 ) {
-                            //on enregistre la dernière forme
-                            vFormes->push_back( forme );
-                            //On va passer à la nouvelle forme
-/*                            cout << "Nouvelle forme" << endl;*/
-                            forme = Forme( 1 );
-                            //on signale qu'on traite une nouvelle forme
-                            nv_forme = 1;
-                        }
+	string ligne;
+	//tant qu'on est pas arrivé à la fin du fichier
+	while ( !fichier.eof() ) {
+	    //on récupère toute la ligne
+	    getline( fichier, ligne );
+	    //on teste la première lettre de la ligne
+	    switch ( ligne[0] ) {
+		//s'il s'agit d'un vertex
+		case 'v':
+		    {
+			//s'il s'agit d'une nouvelle forme
+			if ( nv_forme == 0 ) {
+			    //on enregistre la dernière forme
+			    vFormes->push_back( forme );
+			    //On va passer à la nouvelle forme
+			    /*                            cout << "Nouvelle forme" << endl;*/
+			    forme = Forme( 1 );
+			    //on signale qu'on traite une nouvelle forme
+			    nv_forme = 1;
+			}
 
-/*                        int num_vertice=obj.vertices.size();*/
+			/*                        int num_vertice=obj.vertices.size();*/
 
-                        //je récupère la position du premier espace
-                        pos = ligne.find_first_of(' ');
-                        ligne = ligne.substr( pos+1, ligne.length() );
-                        //je récupère la position du x
-                        pos = ligne.find_first_of(' ');
-                        string l = ligne.substr( 0, pos );
-                        float x = atof(l.c_str());
-                        ligne = ligne.substr( pos+1, ligne.length() );
-                        //je récupère la position du y
-                        pos = ligne.find_first_of(' ');
-                        l = ligne.substr( 0, pos );
-                        float y = atof(l.c_str());
-                        ligne = ligne.substr( pos+1, ligne.length() );
-                        //ce qu'il reste ( c'est le z )
-                        float z = atof(ligne.c_str());
+			//je récupère la position du premier espace
+			pos = ligne.find_first_of(' ');
+			ligne = ligne.substr( pos+1, ligne.length() );
+			//je récupère la position du x
+			pos = ligne.find_first_of(' '); // copier coller ???
+			string l = ligne.substr( 0, pos );
+			float x = atof(l.c_str());
+			ligne = ligne.substr( pos+1, ligne.length() );
+			//je récupère la position du y
+			pos = ligne.find_first_of(' ');
+			l = ligne.substr( 0, pos );
+			float y = atof(l.c_str());
+			ligne = ligne.substr( pos+1, ligne.length() );
+			//ce qu'il reste ( c'est le z )
+			float z = atof(ligne.c_str());
 
-                        Vertex v( cpt_vertex, x, y, z );
+			Vertex v( cpt_vertex, x, y, z );
 
-                        cpt_vertex++;
+			cpt_vertex++;
 
-                        forme.addVertex( v );
-/*                        cout << "vertex = ( " << v.coord[0] << ", " << v.coord[1] << ", " << v.coord[2] << " )" << endl;*/
-                        break;
-                    }
-                    //s'il s'agit d'une face
-                case 'f':
-                    {
-                        //une fois les faces terminées, on passera à une nouvelle forme
-                        nv_forme = 0;
+			forme.addVertex( v );
+			/*                        cout << "vertex = ( " << v.coord[0] << ", " << v.coord[1] << ", " << v.coord[2] << " )" << endl;*/
+			break;
+		    }
+		    //s'il s'agit d'une face
+		case 'f':
+		    {
+			//une fois les faces terminées, on passera à une nouvelle forme
+			nv_forme = 0;
 
-                        //on ajoute une face
-                        Face f( 1 );
+			//on ajoute une face
+			Face f( 1 );
 
-                        //on enleve le "f "
-                        pos = ligne.find_first_of(' ');
-                        ligne = ligne.substr( pos+1, ligne.length() );
-                        pos = ligne.find_first_of(' ');
-                        //tant qu'il y a des espaces
-                        while ( pos > 0 ) {
-                            //je recupere le debut de la ligne jusqu'au premier espace
-                            string l = ligne.substr( 0, pos );
-                            //je transforme le résultat en flottant
-                            f.addSommet( atoi(l.c_str()) );
-                            //je découpe la ligne
-                            ligne = ligne.substr( pos+1, ligne.length() );
-                            //et je cherche l'emplacement du premier espace
-                            pos = ligne.find_first_of(' ');
-                        }
-                        //ce qu'il reste
-                        f.addSommet( atoi(ligne.c_str()) );
+			//on enleve le "f "
+			pos = ligne.find_first_of(' ');
+			ligne = ligne.substr( pos+1, ligne.length() );
+			pos = ligne.find_first_of(' ');
+			//tant qu'il y a des espaces
+			while ( pos > 0 ) {
+			    //je recupere le debut de la ligne jusqu'au premier espace
+			    string l = ligne.substr( 0, pos ); // on redéfinit l ?
+			    //je transforme le résultat en flottant
+			    f.addSommet( atoi(l.c_str()) );
+			    //je découpe la ligne
+			    ligne = ligne.substr( pos+1, ligne.length() );
+			    //et je cherche l'emplacement du premier espace
+			    pos = ligne.find_first_of(' ');
+			}
+			//ce qu'il reste
+			f.addSommet( atoi(ligne.c_str()) );
 
-                        forme.addFace( f );
-                        break;
-                    }
-                    //autrement
-                default:
-                    {
-                        break;
-                    }
-            }
-        }
-        //on enregistre la dernière forme
-        vFormes->push_back( forme );
+			forme.addFace( f );
+			break;
+		    }
+		    //autrement
+		default:
+		    {
+			break;
+		    }
+	    }
+	}
+	//on enregistre la dernière forme
+	vFormes->push_back( forme );
 
-        //on ferme le fichier
-        fichier.close();
-        return 1;
+	//on ferme le fichier
+	fichier.close();
+	return 1;
     }
     //sinon
     else {
-        return 0;
+	return 0;
     }
 }
 
@@ -156,12 +162,12 @@ int parser( string chemin, vector<Forme> * vFormes ) {
  * On dessine pour chaque forme, chacun de ses points
  * On decide du mode de dessin (par exemple GL_LINE_LOOP)
  */
- 
+
 void dessinerScene( vector<Forme> * vFormes ) {
 
     //pour chaque forme
     for ( int i=0; i<vFormes->size(); i++ ) {
-        vFormes->at(i).draw();
+	vFormes->at(i).draw();
     }
 }
 
@@ -180,7 +186,7 @@ void dessiner( vector<Forme> * vFormes ) {
     gluLookAt(xCam,yCam,5,0,0,0,0,1,0);
 
     dessinerScene( vFormes );
-	vFormes->at(0).parcoursGraph();
+    vFormes->at(0).parcoursGraph();
 
     //on s'assure que toutes les commandes OpenGL ont été exécutées
     glFlush();
@@ -208,11 +214,11 @@ void dessinerForme( Forme forme ) {
 }
 
 int main(int argc, char *argv[]){
-	//on vérifie qu'il y est une carte à charger
-	if ( argv[1] == NULL ) {
-		cout << "xx Erreur: Il n'y a pas de carte à charger" << endl;
-		return -1;
-	}
+    //on vérifie qu'il y est une carte à charger
+    if ( argv[1] == NULL ) {
+	cout << "xx Erreur: Il n'y a pas de carte à charger" << endl;
+	return -1;
+    }
 
     //lance SDL
     SDL_Init(SDL_INIT_VIDEO);
@@ -224,7 +230,7 @@ int main(int argc, char *argv[]){
     //fixe la taille de la Fenetre et indique le rendu openGL
     SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32, SDL_OPENGL);
 
-/*    SDL_EnableKeyRepeat(10,10);*/
+    /*    SDL_EnableKeyRepeat(10,10);*/
 
     glMatrixMode( GL_PROJECTION );
     glLoadIdentity( );
@@ -240,87 +246,89 @@ int main(int argc, char *argv[]){
     parser( argv[1], &vFormes );
 
     vFormes.at(0).generateGraph();
-	Vertex * vsuppr = vFormes.at(0).getVertex( 3 );
-	vFormes.at(0).removeSommet( vsuppr );
-/*    vFormes.at(0).parcoursGraph();*/
-/*    cout << vFormes.at(0).printGraph();*/
+    Vertex * vsuppr = vFormes.at(0).getVertex( 3 );
+    vFormes.at(0).removeSommet( vsuppr );
+    /*    vFormes.at(0).parcoursGraph();*/
+    /*    cout << vFormes.at(0).printGraph();*/
 
-	Forme forme;
+    Forme forme;
 
-	vector<Vertex> v;
-	Vertex v1( 1, 1, 0, 0 );
-	v.push_back(v1);
-	Vertex v3( 3, 0, 0, 1 );
-	v.push_back(v3);
-	Vertex v4( 4, 0, 0, 0 );
-	v.push_back(v4);
-	Vertex v2( 2, 1, 0, 1 );
-	v.push_back(v2);
+    vector<Vertex> v;
+    Vertex v1( 1, 1, 0, 0 );
+    v.push_back(v1);
+    Vertex v3( 3, 0, 0, 1 );
+    v.push_back(v3);
+    Vertex v4( 4, 0, 0, 0 );
+    v.push_back(v4);
+    Vertex v2( 2, 1, 0, 1 );
+    v.push_back(v2);
 
-	vector<int> sommets;
-	sommets.push_back(1);
-	sommets.push_back(2);
-	sommets.push_back(3);
-	sommets.push_back(4);
-	Face face( 1 );
-	face.setSommets( &sommets );
+    vector<int> sommets;
+    sommets.push_back(1);
+    sommets.push_back(2);
+    sommets.push_back(3);
+    sommets.push_back(4);
+    Face face( 1 );
+    face.setSommets( &sommets );
 
-	forme.setVertices( &v );
-	forme.addFace( face );
+    forme.setVertices( &v );
+    forme.addFace( face );
 
-	vector<Vertex> vv;
-	Vertex v5( 5, 0, 0, 0 );
-	forme.addVertex(v5);
-	Vertex v6( 6, 0, 1, 0 );
-	forme.addVertex(v6);
-	Vertex v7( 7, 0, 1, 1 );
-	forme.addVertex(v7);
-	Vertex v8( 8, 0, 0, 1 );
-	forme.addVertex(v8);
+    vector<Vertex> vv;
+    Vertex v5( 5, 0, 0, 0 );
+    forme.addVertex(v5);
+    Vertex v6( 6, 0, 1, 0 );
+    forme.addVertex(v6);
+    Vertex v7( 7, 0, 1, 1 );
+    forme.addVertex(v7);
+    Vertex v8( 8, 0, 0, 1 );
+    forme.addVertex(v8);
 
-	vector<int> s2;
-	s2.push_back(5);
-	s2.push_back(6);
-	s2.push_back(7);
-	s2.push_back(8);
-	Face face2( 2 );
-	face2.setSommets( &s2 );
+    vector<int> s2;
+    s2.push_back(5);
+    s2.push_back(6);
+    s2.push_back(7);
+    s2.push_back(8);
+    Face face2( 2 );
+    face2.setSommets( &s2 );
 
-/*	forme.setVertices( &vv );*/
-	forme.addFace( face2 );
+    /*	forme.setVertices( &vv );*/
+    forme.addFace( face2 );
 
-/*    v5.findVoisins( &face2, &forme );*/
-/*    cout << v5.printVoisins() << endl;*/
+    /*    v5.findVoisins( &face2, &forme );*/
+    /*    cout << v5.printVoisins() << endl;*/
 
     while (continuer)
     {
-        dessiner( &vFormes );
-/*		dessinerForme( forme );*/
+	dessiner( &vFormes );
+	/* dessinerForme( forme );*/
 
-        //attente d'évènement bloquante
-        SDL_WaitEvent(&event);
-        switch(event.type) {
-            case SDL_QUIT:
-                continuer = false;
-                break;
+	//Attente d'évènement bloquante
+	SDL_WaitEvent(&event);	
+	switch(event.type) {
+	    // Permet de fermer la fenêtre
+	    case SDL_QUIT:
+		continuer = false;
+		break;
 
-            case SDL_KEYDOWN:
-                switch ( event.key.keysym.sym ) {
-                    case SDLK_UP:
-                        yCam++;
-                        break;
-                    case SDLK_DOWN:
-                        yCam--;
-                        break;
-                    case SDLK_RIGHT:
-                        xCam++;
-                        break;
-                    case SDLK_LEFT:
-                        xCam--;
-                        break;
-                }
-                break;
-        }
+	    // Gestion de la caméra avec la touches directionnelles 
+	    case SDL_KEYDOWN:
+		switch ( event.key.keysym.sym ) {
+		    case SDLK_UP:
+			yCam++;
+			break;
+		    case SDLK_DOWN:
+			yCam--;
+			break;
+		    case SDLK_RIGHT:
+			xCam++;
+			break;
+		    case SDLK_LEFT:
+			xCam--;
+			break;
+		}
+		break;
+	}
 
     }
 
