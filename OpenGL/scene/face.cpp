41b90011a0b92324e30d22e23895584a83e266ac
snipercrazy37 <snipercrazy37@934d30a3-e6d5-50ff-10ec-50884c1415c3>
2010-04-18 16:21:22 +0000

    Rien d'important

--- OpenGL/scene/face.cpp
+++ OpenGL/scene/face.cpp
@@ -1,41 +1,41 @@
 #include "face.h"
 
 Face::Face() {
-	this->num = 0;
-	this->sommets = vector<int>();
+    this->num = 0;
+    this->sommets = vector<int>();
 }
 
 Face::Face( int n) {
-	this->num = n;
+    this->num = n;
 }
 
 int Face::getNum() {
-	return this->num;
+    return this->num;
 }
 
 vector<int> * Face::getSommets(){
-	return &(this->sommets);
+    return &(this->sommets);
 }
 
 void Face::setNum( int n ) {
-	this->num = n;
+    this->num = n;
 }
 
 void Face::setSommets( vector<int> * vSommets ){
-	this->sommets = (* vSommets);
+    this->sommets = (* vSommets);
 }
 
 void Face::addSommet( int num_vertex  ){
-	this->sommets.push_back(num_vertex);
+    this->sommets.push_back(num_vertex);
 }
 
 void Face::removeSommet( int num_vertex ){
 
-	int i=0;
-	while(this->sommets[i]!=num_vertex){
-		i++;
-	}
-	this->sommets.erase(sommets.begin()+i);
+    int i=0;
+    while(this->sommets[i]!=num_vertex){
+	i++;
+    }
+    this->sommets.erase(sommets.begin()+i);
 
 }
 
--- OpenGL/scene/forme.h
+++ OpenGL/scene/forme.h
@@ -12,40 +12,40 @@
 using namespace std;
 
 class Forme {
-	private:
-		int num;
-		vector<Vertex> vertices;
-		vector<Face> faces;
+    private:
+	int num;
+	vector<Vertex> vertices;
+	vector<Face> faces;
 
 
-	public:
-		Forme();
-		Forme( int num );
+    public:
+	Forme();
+	Forme( int num );
 
-		int getNum();
-		vector<Vertex> * getVertices();
-		vector<Face> * getFaces();
-		Vertex * getVertex( int numVertex );
+	int getNum();
+	vector<Vertex> * getVertices();
+	vector<Face> * getFaces();
+	Vertex * getVertex( int numVertex );
 
-		void setNum(int n);
-		void setVertices( vector<Vertex> * vVertex );
-		void setFaces( vector<Face> * vFaces );
+	void setNum(int n);
+	void setVertices( vector<Vertex> * vVertex );
+	void setFaces( vector<Face> * vFaces );
 
-		void addVertex( Vertex v );
-		void addFace( Face f );
+	void addVertex( Vertex v );
+	void addFace( Face f );
 
-		//void removeVertex( int n );
-		//void removeFace( int n );
+	//void removeVertex( int n );
+	//void removeFace( int n );
 
-        void findVoisinsVertex( Vertex * v, Face * f );
-        void generateGraph();
-		void removeSommet( Vertex * v );
-		void parcoursGraph();
+	void findVoisinsVertex( Vertex * v, Face * f );
+	void generateGraph();
+	void removeSommet( Vertex * v );
+	void parcoursGraph();
 
-		void draw();
-		//void drawFace( int num_face );
-		//string toString();
-        string printGraph();
+	void draw();
+	//void drawFace( int num_face );
+	//string toString();
+	string printGraph();
 };
 
 #endif
