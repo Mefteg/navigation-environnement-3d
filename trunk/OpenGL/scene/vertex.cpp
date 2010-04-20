db8a34c66e23ce86c56a3745513d5ef15f3b3a7d
gimenez.tom <gimenez.tom@934d30a3-e6d5-50ff-10ec-50884c1415c3>
2010-04-20 08:02:29 +0000

    La fonction parcoursVoisins() de Vertex ne fait plus qu'un parcours simple ( aucun affichage etc... ), elle ne sert plus que de squelette La fonction parcoursVoisinsDessiner() de Vertex reprends le squelettre de parcoursVoisins() et s'etoffe pour afficher le graphe ( aretes + sommets )

--- trunk/OpenGL/scene/vertex.cpp
+++ trunk/OpenGL/scene/vertex.cpp
@@ -1,35 +1,35 @@
 #include "vertex.h"
 
 Vertex::Vertex() {
-    this->num = 0;
-    this->x = 0;
-    this->y = 0;
-    this->z = 0;
-    this->visite = 0;
+	this->num = 0;
+	this->x = 0;
+	this->y = 0;
+	this->z = 0;
+	this->visite = 0;
 }
 
 Vertex::Vertex( int num, float x, float y, float z ) {
-    this->num = num;
-    this->x = x;
-    this->y = y;
-    this->z = z;
-    this->visite = 0;
+	this->num = num;
+	this->x = x;
+	this->y = y;
+	this->z = z;
+	this->visite = 0;
 }
 
 int Vertex::getNum() {
-    return this->num;
+	return this->num;
 }
 
 float Vertex::getX() {
-    return this->x;
+	return this->x;
 }
 
 float Vertex::getY() {
-    return this->y;
+	return this->y;
 }
 
 float Vertex::getZ() {
-    return this->z;
+	return this->z;
 }
 
 vector<Vertex *> * Vertex::getVoisins() {
@@ -37,23 +37,23 @@ vector<Vertex *> * Vertex::getVoisins() {
 }
 
 int Vertex::getVisite() {
-    return visite;
+	return visite;
 }
 
 void Vertex::setNum( int n ) {
-    this->num = n;
+	this->num = n;
 }
 
 void Vertex::setX( float x ) {
-    this->x = x;
+	this->x = x;
 }
 
 void Vertex::setY( float y ) {
-    this->y = y;
+	this->y = y;
 }
 
 void Vertex::setZ( float z ) {
-    this->z = z;
+	this->z = z;
 }
 
 void Vertex::addVoisin( Vertex * v ) {
@@ -61,7 +61,7 @@ void Vertex::addVoisin( Vertex * v ) {
 }
 
 void Vertex::setVisite( int i ) {
-    this->visite = i;
+	this->visite = i;
 }
 
 void Vertex::removeVoisin( int n ) {
@@ -69,67 +69,90 @@ void Vertex::removeVoisin( int n ) {
     int continuer=1;
     //tant qu'on a rien trouvé et qu'on n'a pas vu tous les voisins
     while ( i < this->getVoisins()->size() && continuer ) {
-	//si c'est le voisin à supprimer
-	if ( this->getVoisins()->at(i)->getNum() == n ) {
-	    //on le supprime
-	    this->getVoisins()->erase(this->getVoisins()->begin()+i);
-	    //et on arrete la recherche
-	    continuer=0;
-	}
-	i++;
+        //si c'est le voisin à supprimer
+        if ( this->getVoisins()->at(i)->getNum() == n ) {
+            //on le supprime
+            this->getVoisins()->erase(this->getVoisins()->begin()+i);
+            //et on arrete la recherche
+            continuer=0;
+        }
+		i++;
     }
 }
 
 bool Vertex::dejaVoisin( int p ) {
     for ( int i=0; i<this->getVoisins()->size(); i++ ) {
-	if ( this->getVoisins()->at(i)->getNum() == p )
-	    return true;
+        if ( this->getVoisins()->at(i)->getNum() == p )
+            return true;
     }
 
     return false;
 }
 
-//Parcours tous les voisins de manière récursive
+//parcours tous les voisins de maniere recursive
 void Vertex::parcoursVoisins() {
-    //Si le sommet n'a pas déjà été visité
-    if ( !this->getVisite() ) {
-	this->setVisite( 1 );
-	this->draw( 255, 0, 0 );
-	for ( int i=0; i<this->voisins.size(); i++ ) {
-	    this->voisins.at(i)->parcoursVoisins();
+	//si le sommet n'a pas deja été visité
+	if ( !this->getVisite() ) {
+		this->setVisite( 1 );
+		for ( int i=0; i<this->voisins.size(); i++ ) {
+			this->voisins.at(i)->parcoursVoisins();
+		}
 	}
-    }
 }
 
-//Retourne 1 si le sommet est isolé, 0 sinon
-    int Vertex::estIsole() {
+//parcours tous les voisins de maniere recursive
+void Vertex::parcoursVoisinsDessiner() {
+	//si le sommet n'a pas deja été visité
+	if ( !this->getVisite() ) {
+		float x = this->getX();
+		float y = this->getY();
+		float z = this->getZ();
+		glVertex3d( x, y, z );
+		glEnd();
+		this->setVisite( 1 );
+		this->draw( 255, 0, 0 );
+		for ( int i=0; i<this->voisins.size(); i++ ) {
+			glLineWidth( 2.0 );
+			glBegin( GL_LINES );
+			glColor3ub( 0, 255, 0 );
+			glVertex3d( x, y, z );
+			this->voisins.at(i)->parcoursVoisinsDessiner();
+		}
+	}
+	else {
+		glEnd();
+	}
+}
+
+//retourne 1 si le sommet est isolé, 0 sinon
+int Vertex::estIsole() {
 	if ( this->getVoisins()->empty() )
-	    return 1;
+		return 1;
 	else
-	    return 0;
-    }
+		return 0;
+}
 
 string Vertex::printVoisins() {
     string str;
     cout << "nb de voisins: " << this->getVoisins()->size() << endl;
     for ( int i=0; i<this->getVoisins()->size(); i++ ) {
-	ostringstream sin;
-	sin << this->getVoisins()->at(i)->getNum();
-	str.append(sin.str());
-	str.append(" ");
+        ostringstream sin;
+        sin << this->getVoisins()->at(i)->getNum();
+        str.append(sin.str());
+        str.append(" ");
     }
 
     return str;
 }
 
 void Vertex::draw( int r, int g, int b ) {
-    float x = this->getX();
-    float y = this->getY();
-    float z = this->getZ();
-    glPointSize( 5.0 );
-    //on crée un glVert
-    glBegin( GL_POINTS );
-    glColor3ub( r, g, b );
-    glVertex3d( x, y, z );
-    glEnd();
+	float x = this->getX();
+	float y = this->getY();
+	float z = this->getZ();
+	glPointSize( 5.0 );
+	//on crée un glVert
+	glBegin( GL_POINTS );
+	glColor3ub( r, g, b );
+	glVertex3d( x, y, z );
+	glEnd();
 }
